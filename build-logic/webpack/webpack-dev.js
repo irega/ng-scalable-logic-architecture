819486606340d63f1a29c75c364377f6ad129238
Ivan Reinoso Garcia <ivan.reinoso@outlook.es>
2018-02-11 12:08:03 +0100

    fixed build-prod script

--- build-logic/webpack/webpack-dev.js
+++ build-logic/webpack/webpack-dev.js
@@ -4,7 +4,6 @@ const base = require('./webpack-base'),
 
 function transformConfig(config, manufacturer) {
     const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
-
     config.devtool = 'source-map';
     config.performance = {
         hints: false
@@ -12,10 +11,8 @@ function transformConfig(config, manufacturer) {
     config.entry['webpack/hot/dev-server'] = 'webpack/hot/dev-server';
     config.entry['webpack-hot-middleware/client'] = 'webpack-hot-middleware/client';
     config.entry.app = process.env.npm_package_config_mainPath;
-
     config.output.filename = manufacturer + '/[name].[hash].bundle.js';
     config.output.chunkFilename = manufacturer + '/[id].[hash].chunk.js';
-
     config.resolve.plugins = [
         new TsConfigPathsPlugin({
             tsconfig: process.env.npm_package_config_tsConfigPath,
@@ -24,36 +21,33 @@ function transformConfig(config, manufacturer) {
     ];
     config.module.rules.push({
         test: /\.ts$/,
-        use: [
-            'awesome-typescript-loader',
-            'angular-router-loader',
-            'angular2-template-loader',
-            'source-map-loader',
-            'tslint-loader'
-        ]
+        use: ['awesome-typescript-loader', 'angular-router-loader', 'angular2-template-loader', 'source-map-loader']
     });
-
+    config.module.rules.push({
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        options: {
+            typeCheck: true
+        }
+    });
+    
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
-    config.plugins.push(
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            template: process.env.npm_package_config_indexPath
-        }));
-
+    config.plugins.push(new HtmlWebpackPlugin({
+        inject: 'body',
+        template: process.env.npm_package_config_indexPath
+    }));
     return config;
 }
-
 module.exports = {
-    startServer: function (manufacturer) {
+    startServer: function(manufacturer) {
         var baseConfig = base.getConfig();
         var config = transformConfig(Object.create(baseConfig), manufacturer);
-
         const express = require('express');
         const webpackDevMiddleware = require('webpack-dev-middleware');
         const webpackHotMiddleware = require('webpack-hot-middleware');
         const app = express();
         const compiler = webpack(config);
-
         app.use(webpackDevMiddleware(compiler, {
             stats: {
                 colors: true
@@ -61,9 +55,8 @@ module.exports = {
             publicPath: config.output.publicPath
         }));
         app.use(webpackHotMiddleware(compiler));
-
         const port = process.env.npm_package_config_devServerPort;
-        app.listen(port, function () {
+        app.listen(port, function() {
             console.log('Wepack DevServer listening on port ' + port + '!\n');
         });
     }
--- build-logic/webpack/webpack-prod.js
+++ build-logic/webpack/webpack-prod.js
@@ -2,25 +2,47 @@ const base = require('./webpack-base'),
     path = require('path'),
     gutil = require('gulp-util'),
     webpack = require('webpack'),
+    ngToolsWebpack = require('@ngtools/webpack'),
     HtmlWebpackPlugin = require('html-webpack-plugin');
 
 function transformConfig(config, manufacturer) {
+    const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
+    config.entry.app = process.env.npm_package_config_mainAotPath;
     config.output.filename = '[name].[hash].bundle.js';
     config.output.chunkFilename = '[id].[hash].chunk.js';
     config.output.path = path.resolve(process.env.npm_package_config_distPath, '.') + '/' + manufacturer;
-
-    config.plugins.push(
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            template: process.env.npm_package_config_indexPath
-        }));
+    config.resolve.plugins = [
+        new TsConfigPathsPlugin({
+            tsconfig: process.env.npm_package_config_tsConfigAotPath,
+            compiler: 'typescript'
+        })
+    ];
+    config.module.rules.push({
+        test: /\.ts$/,
+        loader: '@ngtools/webpack'
+    });
+    config.plugins.push(new ngToolsWebpack.AotPlugin({
+        tsConfigPath: process.env.npm_package_config_tsConfigAotPath
+    }));
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        output: {
+            comments: false
+        },
+        sourceMap: false
+    }));
+    config.plugins.push(new HtmlWebpackPlugin({
+        inject: 'body',
+        template: process.env.npm_package_config_indexPath
+    }));
     return config;
 }
-
 module.exports = {
-    start: function (manufacturer, callback) {
+    start: function(manufacturer, callback) {
         var baseConfig = base.getConfig();
-        webpack(transformConfig(Object.create(baseConfig), manufacturer), function (err, stats) {
+        webpack(transformConfig(Object.create(baseConfig), manufacturer), function(err, stats) {
             if (err) throw new gutil.PluginError('webpack-prod', err);
             gutil.log('[webpack-prod]', stats.toString({
                 colors: true
